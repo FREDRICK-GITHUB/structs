8370f9077432ac89e9beb21e271d078510b38244
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-12 08:22:42 +0300

    struct default structure added

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+
+    let mut user1 = User {
+        username: String::from("fredrick"),
+        email: String::from("fredmainahk@gmail.com"),
+        sign_in_count: 1,
+        active: true,
+    };
+
+    user1.sign_in_count = 3;
+
+    println!("my username is: {}", user1.username);
+    println!("my email address is: {}", user1.email);
+    println!("my sign in count is: {}", user1.sign_in_count);
+    println!("my status is: {}", user1.active);
+}
