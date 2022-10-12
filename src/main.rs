bab019489236c0ec6d26f4b6c85d73ff1aa14b0c
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-12 08:50:15 +0300

    added function that returns a struct within the main function

--- src/main.rs
+++ src/main.rs
@@ -20,4 +20,19 @@ fn main() {
     println!("my email address is: {}", user1.email);
     println!("my sign in count is: {}", user1.sign_in_count);
     println!("my status is: {}", user1.active);
+
+    fn build_user(email: String, username: String) -> User {
+        User {
+            email: email,
+            username: username,
+            sign_in_count: 1,
+            active: true,
+        }
+    }
+
+    let results =  build_user(user1.email, user1.username);
+    println!("I know this is not the best example but the function results are: {:?}, {:?}, {:?}, {:?}", results.username, results.email, results.active, results.sign_in_count);
+   
 }
+
+
