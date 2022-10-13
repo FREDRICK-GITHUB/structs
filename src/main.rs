b94926cfb2482630d4dac82dff6540c05b117407
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-13 08:03:13 +0300

    added two examples of tuple structs

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,19 @@
 fn main() {
-    struct User {
-        username: String,
-        email: String,
-        sign_in_count: u64,
-        active: bool,
-    }
+    /*
+    Tuple structs have unnamed fields.
+    Their type is declared but no value names are given
 
-    let mut user1 = User {
-        username: String::from("fredrick"),
-        email: String::from("fredmainahk@gmail.com"),
-        sign_in_count: 1,
-        active: true,
-    };
-
-    //this struct shows how we can use some values that were declared in another struct instance by directly referencing that struct
-    let mut user2 = User {
-        email: String::from("fredmainahk"),
-        username: String::from("freddie"),
-        sign_in_count: user1.sign_in_count,
-        active: user1.active,
-    };
-
-    //this implementation show how we can update only some values and reuse others from another struct
-    let mut user3 = User {
-        username: String::from("maina"),
-        email: String::from("kamaufredm@gmail.com"),
-        ..user1
-    };
-
-    println!("We have two users with 3 user names which are: {} and: {} aand: {}", user1.username, user2.username, user3.username);
-
+    */  
     
+    //tuple struct declaration
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    println!("the first example of a tuple struct values for black color is: {} {} {}", black.0, black.1, black.2);
+    println!("the three points declared are: {} {} {}",origin.0, origin.1, origin.2);
    
 }
 
